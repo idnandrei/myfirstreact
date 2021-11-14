a8cc1b0de3aabab4723c3baae14ab106858abfff
idnandrei <idanandrei.paguio.iics@ust.edu.ph>
2021-11-14 18:53:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EICCIN
+          EICCIN 1234
         </p>
       </header>
     </div>
