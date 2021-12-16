6e81f4babb47d8a3a02d5bf0a997c9a269752c99
idnandrei <idanandrei.paguio.iics@ust.edu.ph>
2021-12-16 13:50:51 +0800

    Update httpclient.js

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://employee-list-backend.herokuapp.com/api/v1/employees",
+  baseURL: "https://employee-list-backend.herokuapp.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
